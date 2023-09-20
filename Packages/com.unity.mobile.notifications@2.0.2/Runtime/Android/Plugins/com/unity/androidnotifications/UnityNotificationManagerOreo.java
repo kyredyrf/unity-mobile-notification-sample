2e2317f45b50aacae748a597f90b8d06e7547f73
kyredyrf <kyredyrf@yahoo.co.jp>
2023-09-21 01:33:04 +0900

    Android通知音差し替え

--- Packages/com.unity.mobile.notifications@2.0.2/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManagerOreo.java
+++ Packages/com.unity.mobile.notifications@2.0.2/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManagerOreo.java
@@ -2,13 +2,27 @@ package com.unity.androidnotifications;
 
 import android.app.Activity;
 import android.app.NotificationChannel;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
 
+import com.unity3d.player.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UnityNotificationManagerOreo extends UnityNotificationManager {
+    final String TAG = "sample";
+
     public UnityNotificationManagerOreo(Context context, Activity activity) {
         super(context, activity);
     }
@@ -36,7 +50,65 @@ public class UnityNotificationManagerOreo extends UnityNotificationManager {
         channel.setVibrationPattern(vibrationPattern);
         channel.setLockscreenVisibility(lockscreenVisibility);
 
+        int soundId = R.raw.sample_notify;
+        setSound(channel, soundId);
+        exportSound(soundId);
+
         getNotificationManager().createNotificationChannel(channel);
+    }
+
+    private void setSound(NotificationChannel channel, int soundId) {
+        // 通知音差し替え
+        StringBuilder sb = new StringBuilder()
+            .append(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .append(File.pathSeparator)
+            .append(File.separator)
+            .append(File.separator)
+            .append(mContext.getPackageName())
+            .append(File.separator)
+            .append(mContext.getResources().getResourceTypeName(soundId))
+            .append(File.separator)
+            .append(mContext.getResources().getResourceEntryName(soundId));
+        Uri uri = Uri.parse(sb.toString());
+        AudioAttributes attributes = new AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build();
+        channel.setSound(uri, attributes);
+    }
+
+    private void exportSound(int soundId) {
+        // 通知音コピー
+        try {
+            File dir = new File(mContext.getExternalMediaDirs()[0], Environment.DIRECTORY_NOTIFICATIONS);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            InputStream inputStream = mContext.getResources().openRawResource(soundId);
+            String name = mContext.getResources().getResourceEntryName(soundId) + ".wav";
+            Log.i(TAG, "export " + name + " to " + dir.toString() + ".");
+
+            File path = new File(dir.toString() + File.separator + name);
+            FileOutputStream outputStream = new FileOutputStream(path, false);
+            int DEFAULT_BUFFER_SIZE = 1024 * 4;
+            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+            int size = 0;
+            while ((size = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, size);
+            }
+            inputStream.close();
+            outputStream.close();
+
+            MediaScannerConnection.scanFile(
+                mContext,
+                new String[] { path.toString() },
+                new String[] { "audio/wav" },
+                (String scannedPath, Uri scannedUri) -> {
+                });
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
     }
 
     protected static NotificationChannelWrapper getOreoNotificationChannel(Context context, String id) {
